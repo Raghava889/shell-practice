401b61d28620701f9055c40747c73b21287b81c7
Raghava889 <rm48360@gmail.com>
2026-08-04 10:29:12 +0530

    second

---
+++ 04.variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your name::"
+read USER_NAME #here USER_NAME is variable whatever you enter in terminal stores in this variable
+
+echo "Username is $USER_NAME"
+
+echo "Please enter the password::"
+read -s PASSWORD
