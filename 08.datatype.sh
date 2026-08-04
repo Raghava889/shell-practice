7e0dc89dd2ed9d7953e12fcfc0438f97b5ac158c
Raghava889 <rm48360@gmail.com>
2026-08-04 13:06:03 +0530

    second

--- 08.datatype.sh
+++ 08.datatype.sh
@@ -2,14 +2,15 @@
 
 NUMBER=$1
 
-# -gt - greater than
-# -lt - less than
-# -eq - equal
-# -ne - not equal
-# -ge - greater than or equal
-# -le - less than or equal
+# gt - greater than
+# lt - less than
+# eq - equal
+# ne - not equal
+# ge - greater than or equal
+# le - less than or equal
 
 if [ $NUMBER -ge 20 ]; then
     echo "Given number $NUMBER is greater than or equal to 20"
+else
     echo "Given number $NUMBER is less than 20"
 fi
