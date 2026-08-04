419911faa8a5aa30286c29f8913bb73a26666fce
Raghava889 <rm48360@gmail.com>
2026-08-04 10:17:13 +0530

    second

---
+++ 03.variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#we are going to pass values to variables through command line args
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi $Person2 i am going to blast you"
+echo "$Person2: Hey $Person1 you are welcome"
+echo "$Person1: $Person2 i am very serious"
+echo "$Person2: I am also serius please welcome"
