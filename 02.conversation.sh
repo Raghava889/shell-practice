90a0c0bd186e9f851a9dbf4d0f2ae45a1d445947
Raghava889 <rm48360@gmail.com>
2026-08-04 10:01:46 +0530

    second

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hi Iran i am going to blast you"
+echo "Iran: Hey Trump you are welcome"
+echo "Trump: Iran i am very serious"
+echo "Iran: I am also serius please welcome"
