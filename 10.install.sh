cbd28536704fd1a83b0bcd821eb00d32ae447d9b
Raghava889 <rm48360@gmail.com>
2026-08-05 11:27:46 +0530

    second

--- 10.install.sh
+++ 10.install.sh
@@ -2,41 +2,29 @@
 
 USERID=$(id -u)
 
-# Check root access or not
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root access"
-    exit 1
-fi
 
-# first arg -> what are you trying to install
-# second arg -> exit code
-#$1 and $2 special variables
+echo "Please get the root access"
+
+exit 1
+
+fi
 
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "Installing $1 is ... FAILED"
-        exit 1
+    echo "installing $1 is failed"
+    exit 1
+
     else
-        echo "Installing $1 is ... SUCCESS"
-    fi
+    echo "installing $1 is success"
+fi
 }
 
-# echo "I am continuing..."
 dnf list installed mysql
 
 if [ $? -eq 0 ]; then
-    echo "MySQL is already installed ... SKIPPING"
+echo "mysql is already installed"
 else
-    echo "Installing MySQL"
-    dnf install mysql -y
-    VALIDATE MySQL $?
-fi
-
-dnf list installed nginx
-if [ $? -eq 0 ]; then
-    echo "nginx is already installed ... SKIPPING"
-else
-    echo "Installing nginx"
-    dnf install nginx -y
-    VALIDATE nginx $?
+dnf install mysql -y
+VALIDATE mysql
 fi
