cdbd37cdb17cc11f6dc8fdf23b1f12cfdcc125b9
Raghava889 <rm48360@gmail.com>
2026-08-10 10:22:19 +0530

    second

--- 12.count.sh
+++ 12.count.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-TIME_STAMP=$(date +%s)
 
-echo "Today is $TIME_STAMP"
 
 START_TIME=$(date +%s)
 
@@ -10,6 +8,6 @@ sleep 10
 
 END_TIME=$(date +%s)
 
-TOTAL_TIME=(($END_TIME-$START_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "This is the Total time $TOTAL_TIME second"
