bd329becbe76e37858baef324b3669f1fc482476
Raghava889 <rm48360@gmail.com>
2026-08-10 17:10:10 +0530

    second

--- 12.count.sh
+++ 12.count.sh
@@ -3,10 +3,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
 echo "The given $NUMBER is grater then 20"
-elif 
-if [ $NUMBER -eq 20 ]; then
+elif [ $NUMBER -eq 20 ]; then
 echo "The given $NUMBER is qual to 20"
 else 
 echo "The given $NUMBER is less then 20"
-    fi
  fi   
