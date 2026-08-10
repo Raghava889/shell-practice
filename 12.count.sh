07f1555b52fc2c983c826335d3c49525d510761a
Raghava889 <rm48360@gmail.com>
2026-08-10 10:40:21 +0530

    second

--- 12.count.sh
+++ 12.count.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-echo "Please enter the user name:"
-
-
-
-echo "User name is $USER_NAME"
-
-echo "Please enter the password::"
-
-read -s PASSWORD
-
-echo "Password is $PASSWORD"
-
+echo "Please list the script name $0"
