1e2c6dcc525565644c27ad404435caf6282c5b3c
Raghava889 <rm48360@gmail.com>
2026-08-11 10:05:17 +0530

    second

--- 12.count.sh
+++ 12.count.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
+LOG_DIR=/var/log/shell-script
+LOG_FILE=$LOG_DIR/$0.log
 
-USERID=$(id -u)
-LOGS_DIR=/var/log/shell-script
-LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
-
-if [ $USERID -ne 0 ]; then
-
-echo "Please get the root access and try the installation...skipping"
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+echo "Please get the root access for this task"
 exit 1
-
 fi
 
-Raghava(){
+VALIDATE(){
     if [ $2 -ne 0 ]; then
-    echo "Installing $1 is faild"
-    exit 1
+    echo "Installing $1 is failed"
     else
     echo "Installing $1 is success"
-
-    fi
+fi
 }
- 
- dnf list installed mysql &>> $LOGS_FILE
- if [ $? -ne 0 ]; then
- echo "package not installed now installing the package"
- dnf install mysql -y &>> $LOGS_FILE
- Raghava "mysql" $?
- else
- echo "Package already installed"
- fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+echo "Package not installed and installing now"
+dnf install mysql -y
+VALIDATE "mysql" $?
+fi
