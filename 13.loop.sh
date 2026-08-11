528ca786486fbc2405298c91d9dc0ecae2ded2d0
Raghava889 <rm48360@gmail.com>
2026-08-11 19:54:51 +0530

    second

--- 13.loop.sh
+++ 13.loop.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for i in {1..100}
-do
-echo $i
-done
+aws ec2 run-instances \
+    --image-id ami-0220d79f3f480ecf5 \
+    --instance-type t3.micro \
+    --security-groups roboshop-common \ #security group name istunna so we have to use (--security-groups) if we are giving ids --security-group-ids
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Install-Server}]' 
+    --query "Instances[0].InstanceId" \ ###search in google - aws cli to launch instance and get instance id
+    --output text
