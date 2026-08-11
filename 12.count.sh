36fbbb993b3c10d08ff9bac46ac47c671cf9e59f
Raghava889 <rm48360@gmail.com>
2026-08-11 09:47:02 +0530

    second

--- 12.count.sh
+++ 12.count.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-NUMBER=$1
 
-if [ $NUMBER -gt 20 ]; then
-echo "The given number $NUMBER is grater then 20"
-elif [ $NUMBER -eq 20 ]; then
-echo "The given number $NUMBER is qual to 20"
-else 
-echo "The given number $NUMBER is less then 20"
- fi   
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+
+echo "Please get the root access and try the installation...skipping"
+exit 1
+
+fi
+
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+    echo "Installing $1 is faild"
+    exit 1
+    else
+    echo "Installing $1 is success"
+
+    fi
+}
+ 
+ dnf list installed mysql
+ if [ $? -ne 0 ]; then
+ echo "package not installed now installing the package"
+ dnf install mysql -y
+ VALIDATE "MYSQL" $?
+ else
+ echo "Package already installed"
+ fi
