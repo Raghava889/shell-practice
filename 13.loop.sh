48f807d0475a3a6bcd5cdf32660e268fa43552fa
Raghava889 <rm48360@gmail.com>
2026-08-12 12:05:15 +0530

    second

--- 13.loop.sh
+++ 13.loop.sh
@@ -1,9 +1,56 @@
 #!/bin/bash
 
-aws ec2 run-instances \
-    --image-id ami-0220d79f3f480ecf5 \
-    --instance-type t3.micro \
-    --security-groups roboshop-common \ #security group name istunna so we have to use (--security-groups) if we are giving ids --security-group-ids
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Install-Server}]' 
-    --query "Instances[0].InstanceId" \ ###search in google - aws cli to launch instance and get instance id
-    --output text
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z0525807R99HNPEWYRKU"
+DOMAIN_NAME="makkena.shop"
+
+for instance in "$@"
+do
+    echo "Launching instance: $instance"
+    
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-groups "roboshop-common" "roboshop-$instance" \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="roboshop-'"$instance"'"}]' \
+        --query "Instances[0].InstanceId" \
+        --output text)
+        
+    echo "Instance Id: $INSTANCE_ID"
+
+    if [ "$instance" == "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[*].Instances[*].PublicIpAddress' \
+            --output text)
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+            --output text)
+        R53_RECORD="$instance.$DOMAIN_NAME"
+    fi
+
+    #### Updating R53 Record ####
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id "$ZONE_ID" \
+        --change-batch '
+        {
+            "Comment": "Update A record to new IP",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'"$R53_RECORD"'",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'"$IP"'"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        '
+done
